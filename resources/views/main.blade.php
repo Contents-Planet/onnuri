c0a274ab97ade46a44766a7f3d36e47082ac5a5d
youngjin <jyj@ct-planet.co.kr>
2023-11-15 10:40:00 +0900

    feat : app layout add

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,33 +1,5 @@
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-  <meta charset="UTF-8">
-  <title>온누리상품권</title>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
-  <meta name="description" content="이제 온라인몰 '온누리시장'에서도 온누리상품권으로 결제하세요!"/>
-  <meta name="keywords" content="KT, 온누리, 온누리상품권, 온라인결제, 온누리몰, 전통시장, 온누리결제, 온누리이벤트, 온누리온라인결제, 온누리인터넷, 온누리상품권앱, 온누리결제"/>
-  <meta property='og:site_name' content='온누리상품권'/>
-  <meta property="og:title" content="온누리상품권"/>
-  <meta property="og:description" content="이제 온라인몰 '온누리시장'에서도 온누리상품권으로 결제하세요!"/>
-  <meta property="og:url" content=""/>
-  <meta property="og:type" content="article"/>
-
-  <meta property="og:type" content="article"/>
-  <meta property="og:image" content="https://static.econtents.co.kr/_img/onnuri/og_img.jpg">
-  <meta property="og:image:width" content="600">
-  <meta property="og:image:height" content="315">
-
-  <link rel="icon" type="image/png" sizes="32x32" href="https://static.econtents.co.kr/_img/onnuri/favicon_v2.png"/>
-
-  <link rel="stylesheet" href="/assets/css/common.css?v=20230921" type="text/css"/>
-  <link rel="stylesheet" href="/assets/css/style.css?v=20230921" type="text/css"/>
-
-  <script src="//static.econtents.co.kr/_asset/_lib/jquery-3.2.1.min.js"></script>
-  <script src="//static.econtents.co.kr/_asset/_lib/cssua.min.js"></script>
-  <script src="//static.econtents.co.kr/_asset/_lib/jquery-images-loaded.min.js"></script>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
 <div id="sticky-line"></div>
 <div id="wrap">
   <div id="container">
@@ -96,6 +68,8 @@
     </div>
   </footer>
 </div>
+@endsection
+@push("js")
 <script>
   $(function () {
     Page.Init();
@@ -130,8 +104,7 @@
     }
   }
 </script>
-</body>
-</html>
+@endpush
 
 
 
